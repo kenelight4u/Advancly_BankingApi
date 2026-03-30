c1e8ce60aebb690687734f8cefccc54fdad557c6
eanolue@sbsc.com <eanolue@sbsc.com>
2026-03-30 12:06:05 +0100

    application is starting now

--- src/BankingApi.Api/Program.cs
+++ src/BankingApi.Api/Program.cs
@@ -225,7 +225,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking API v1");
-        options.RoutePrefix = string.Empty; // Swagger at root "/"
+        options.RoutePrefix = string.Empty;
         options.DisplayRequestDuration();
         options.EnableDeepLinking();
     });
